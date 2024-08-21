5297732cbf609af320727c72bdf8eb74f807d176
ChongKaKam <zhuangjx23@mails@tsinghua.edu.cn>
2024-08-21 15:20:58 +0800

    Update: New ChatGPT completed by OpenAI package

---
+++ BigModel/OpenAI/GPT4o-langchain.py
@@ -0,0 +1,47 @@
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+from BigModel.Base import BigModelBase
+
+'''
+Genernal GPT-4o
+'''
+class Chat_GPT4o(BigModelBase):
+    def __init__(self, max_tokens=1024, temperature=0.1, top_p=0.5):
+        super().__init__(max_tokens, temperature, top_p)
+        self.load_my_api('openai')
+        os.environ['OPENAI_API_KEY'] = self.api_key
+        self.model = ChatOpenAI(model='gpt-4o-2024-08-06', max_tokens=self.max_tokens, temperature=self.temperature, top_p=self.top_p)
+
+    def chat(self, content:list):
+        message = HumanMessage(content=content)
+        response = self.model.invoke([message])
+        return response
+'''
+GPT-4o with structured output
+'''
+class Chat_GPT4o_Structured(BigModelBase):
+    '''
+    Structured output for GPT-4o
+    '''
+    class StructuredModel(BaseModel):
+        # trend: str = Field(description="The trend of the time series")
+        # seasonality: str = Field(description="The seasonality of the time series")
+        abnormal_index: str = Field(description="the output format should be like '[(start1, end1), (start2, end2), ...]', if there are some single outliers, the output should be '[(index1), (index2), ...]',if there is no abnormality, you can say '[]'. The final output should can be mixed with these three formats.")
+        abnormal_type: str = Field(description="The abnormality type of the time series, choose from [none, shapelet, seasonal, trend].")
+        abnormal_description: str = Field(description="Make a brief description of the abnormality, why do you think it is abnormal?")
+
+    def __init__(self, max_tokens=1200, temperature=0.1, top_p=0.5):
+        super().__init__(max_tokens, temperature, top_p)
+        self.load_my_api('openai')
+        os.environ['OPENAI_API_KEY'] = self.api_key
+        general_model = ChatOpenAI(model='gpt-4o-2024-08-06', max_tokens=self.max_tokens, temperature=self.temperature, top_p=self.top_p)
+        self.model = general_model.with_structured_output(self.StructuredModel)
+
+    def chat(self, content:list):
+        message = HumanMessage(content=content)
+        response = self.model.invoke([message])
+        return response
+
--- BigModel/OpenAI/GPT4o.py
+++ BigModel/OpenAI/GPT4o.py
@@ -1,47 +1,63 @@
-import os
-from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
-from langchain_core.pydantic_v1 import BaseModel, Field
+from openai import OpenAI
+from pydantic import BaseModel, Field
 
 from BigModel.Base import BigModelBase
 
 '''
-Genernal GPT-4o
+General GPT-4o
 '''
 class Chat_GPT4o(BigModelBase):
     def __init__(self, max_tokens=1024, temperature=0.1, top_p=0.5):
         super().__init__(max_tokens, temperature, top_p)
         self.load_my_api('openai')
-        os.environ['OPENAI_API_KEY'] = self.api_key
-        self.model = ChatOpenAI(model='gpt-4o-2024-08-06', max_tokens=self.max_tokens, temperature=self.temperature, top_p=self.top_p)
+        self.client = OpenAI(api_key=self.api_key)
+        self.model_name = 'gpt-4o-2024-08-06'
+        self.used_token = {
+            'completion_tokens': 0,
+            'prompt_tokens': 0,
+            'total': 0
+        }
 
-    def chat(self, content:list):
-        message = HumanMessage(content=content)
-        response = self.model.invoke([message])
-        return response
+    def chat(self, message:list):
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=message,
+            max_tokens=self.max_tokens,
+            temperature=self.temperature,
+            top_p=self.top_p
+        )
+        ans = response.choices[0].message.content
+        self.used_token['completion_tokens'] += response.usage.completion_tokens
+        self.used_token['prompt_tokens'] += response.usage.prompt_tokens
+        self.used_token['total_tokens'] += response.usage.total_tokens
+        return ans
+    
 '''
 GPT-4o with structured output
 '''
 class Chat_GPT4o_Structured(BigModelBase):
-    '''
-    Structured output for GPT-4o
-    '''
-    class StructuredModel(BaseModel):
-        # trend: str = Field(description="The trend of the time series")
-        # seasonality: str = Field(description="The seasonality of the time series")
-        abnormal_index: str = Field(description="the output format should be like '[(start1, end1), (start2, end2), ...]', if there are some single outliers, the output should be '[(index1), (index2), ...]',if there is no abnormality, you can say '[]'. The final output should can be mixed with these three formats.")
-        abnormal_type: str = Field(description="The abnormality type of the time series, choose from [none, shapelet, seasonal, trend].")
-        abnormal_description: str = Field(description="Make a brief description of the abnormality, why do you think it is abnormal?")
-
     def __init__(self, max_tokens=1200, temperature=0.1, top_p=0.5):
         super().__init__(max_tokens, temperature, top_p)
         self.load_my_api('openai')
-        os.environ['OPENAI_API_KEY'] = self.api_key
-        general_model = ChatOpenAI(model='gpt-4o-2024-08-06', max_tokens=self.max_tokens, temperature=self.temperature, top_p=self.top_p)
-        self.model = general_model.with_structured_output(self.StructuredModel)
-
-    def chat(self, content:list):
-        message = HumanMessage(content=content)
-        response = self.model.invoke([message])
-        return response
-
+        self.client = OpenAI(api_key=self.api_key)
+        self.model_name = 'gpt-4o-2024-08-06'
+        self.used_token = {
+            'completion_tokens': 0,
+            'prompt_tokens': 0,
+            'total_tokens': 0
+        }
+    def chat(self, message:list, FormatModel:BaseModel):
+        response = self.client.beta.chat.completions.parse(
+            model=self.model_name,
+            messages=message,
+            max_tokens=self.max_tokens,
+            temperature=self.temperature,
+            top_p=self.top_p,
+            response_format=FormatModel,
+        )
+        ans = response.choices[0].message.parsed
+        # count the used tokens
+        self.used_token['completion_tokens'] += response.usage.completion_tokens
+        self.used_token['prompt_tokens'] += response.usage.prompt_tokens
+        self.used_token['total_tokens'] += response.usage.total_tokens
+        return ans
